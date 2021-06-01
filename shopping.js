c06070a814c9e5937d46702a792e3674c8f9838c
hcrudge <hcrudge1@gmail.com>
2021-06-01 11:43:34 -0500

    ShoppingListInitial

---
+++ shopping.js
@@ -0,0 +1,38 @@
+const shoppingList = [ 
+    {
+        item: "milk", 
+        UOM: "1/2 gallon",
+        price: 1.75,
+        quantity: 1
+    },
+    {
+        item: "eggs",
+        UOM: "1 dozen",
+        price: 1,
+        quantity:1
+    },
+    {
+        item: "half & half",
+        UOM: "1 quart",
+        price: 2.05,
+        quantity: 2
+    },
+    {
+        item: "ground beef",
+        UOM: "1 pound",
+        price: 3.50,
+        quantity: 1
+    },
+    {
+        item: "onions",
+        UOM: "3 pound",
+        price: 1.50,
+        quantity: 1
+    }
+]
+console.log(shoppingList)
+
+
+    
+    
+
